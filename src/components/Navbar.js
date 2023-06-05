656e1c7aeb85181b8a624a45ff44440b6512f956
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-06-05 14:03:21 -0600

    Add Hamburguer menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,7 @@
+import { useEffect, useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { MdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
 
 const links = [
   { path: '/', text: 'Home' },
@@ -7,13 +10,40 @@ const links = [
   { path: 'login', text: 'Login' },
 ];
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (navbarOpen && ref.current && !ref.current.contains(event.target)) {
+        setNavbarOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handler);
+    return () => {
+      document.removeEventListener('mousedown', handler);
+    };
+  }, [navbarOpen]);
+
+  const menuStyle = { width: '33px', height: '33px' };
+
   return (
-    <nav className="navbar">
-      <ul>
+    <nav ref={ref} className="navbar">
+      <button className="toggle" onClick={() => setNavbarOpen((prev) => !prev)}>
+        {navbarOpen ? (
+          <MdClose style={menuStyle} />
+        ) : (
+          <FiMenu style={menuStyle} />
+        )}
+      </button>
+      <ul className={`menu-nav${navbarOpen ? ' show-menu' : ''}`}>
         {links.map((link) => {
           return (
             <li key={link.text}>
-              <NavLink to={link.path}>{link.text}</NavLink>
+              <NavLink to={link.path} onClick={() => setNavbarOpen(false)}>
+                {link.text}
+              </NavLink>
             </li>
           );
         })}
